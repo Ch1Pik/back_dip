91227519c45940c620526bcffa4bd55a5c915d9b
Sergey Novikov <149697882+Ch1Pik@users.noreply.github.com>
2024-03-27 09:58:07 +0300

    Update serializers.py

--- posts /serializers.py
+++ posts /serializers.py
@@ -1,9 +1,22 @@
 from rest_framework import serializers
 
 
+from .models import Post, Comment
+
 class CommentSerializer(serializers.ModelSerializer):
-    ...
+    class Meta:
+        model = Comment
+        fields = "__all__"
+        read_only_fields = ["user", ]
 
 
 class PostSerializer(serializers.ModelSerializer):
-    ...
+    class Meta:
+        model = Post
+        fields = "__all__"
+        read_only_fields = ["user", ]
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['comments'] = CommentSerializer(instance.comments, many=True).data
+        return representation
